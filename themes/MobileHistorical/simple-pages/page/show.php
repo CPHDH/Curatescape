6bd472ef627ff1dbf94277601e62b5890cd97eb5
Erin Bell <ebellempire@gmail.com>
2013-03-15 14:05:26 -0400

    Updates AddThis for tour and page templates
    See earlier commit for items pages update

--- themes/MobileHistorical/simple-pages/page/show.php
+++ themes/MobileHistorical/simple-pages/page/show.php
@@ -96,15 +96,15 @@ if (simple_pages_is_home_page(get_current_simple_page())) {
 			<h3 style="margin-top:10px;clear:both">Share this Page</h3>
 			<!-- AddThis Button BEGIN -->
 			<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-			<a class="addthis_button_preferred_1"></a>
-			<a class="addthis_button_preferred_2"></a>
-			<a class="addthis_button_preferred_3"></a>
-			<a class="addthis_button_preferred_4"></a>
-		</div>
-		<?php $addthis = (get_theme_option('Add This')) ? (get_theme_option('Add This')) : 'ra-4e89c646711b8856';?>
-		<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=<?php echo $addthis ;?>"></script>
-			<!-- replace #pubid= value with your ADDTHIS user profile to enable analytics (see settings >> profiles) -->
-		<!-- AddThis Button END -->
+				<a class="addthis_button_preferred_2"></a>
+				<a class="addthis_button_preferred_3"></a>
+				<a class="addthis_button_preferred_4"></a>
+				<a class="addthis_button_compact"></a>
+			</div>
+			<?php $addthis = (get_theme_option('Add This')) ? (get_theme_option('Add This')) : 'ra-4e89c646711b8856';?>
+			<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+			<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=<?php echo $addthis ;?>"></script>
+			<!-- AddThis Button END -->	
 	</div>
 	</div>
 	
