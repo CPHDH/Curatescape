9287c50bfe49ba2274e54dc618908dbfa58bbac3
Erin Bell <ebellempire@gmail.com>
2013-04-15 14:02:18 -0400

    Tour Builder: LatLon for items
    Adds latlon for each item in tour to output for tours.show

--- plugins/MobileJson/views/shared/tours/show.mjson.php
+++ plugins/MobileJson/views/shared/tours/show.mjson.php
@@ -3,9 +3,12 @@
 // Add enumarations of the ordered items in this tour.
 $items = array();
 foreach( $tour->Items as $item ) {
+$location = geolocation_get_location_for_item($item->id, true);
    $item_metadata = array(
       'id'     => $item->id,
       'title'  => $this->itemMetadata( $item, 'Dublin Core', 'Title' ),
+      'latitude' => $location['latitude'],
+      'longitude' => $location['longitude']
    );
 
    array_push( $items, $item_metadata );
