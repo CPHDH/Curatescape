e1cf97d3bfe2bd1782110e1b6a7c9dab9ac5af4f
Erin Bell <ebellempire@gmail.com>
2026-07-14 12:26:03 -0400

    shim css for omeka versions <= 3.2.1

--- views/helpers/FilterBodyTagAttributes.php
+++ views/helpers/FilterBodyTagAttributes.php
@@ -1,9 +1,15 @@
 <?php
 class Curatescape_View_Helper_FilterBodyTagAttributes extends Zend_View_Helper_Abstract{
 	// if .curatescape-fix-[themeName] class is present, use theme-specific css fixes
+	// admin gets a version class so stylesheets can shim markup differences in older core themes
 	public function FilterBodyTagAttributes($attributes)
 	{
-		if(is_admin_theme()) return $attributes;
+		if(is_admin_theme()){
+			if(version_compare(OMEKA_VERSION, '3.2.1', '<')){
+				$attributes['class'] = $attributes['class'].' curatescape--is-omeka-version-lte-3-2';
+			}
+			return $attributes;
+		}
 		$attributes['class'] = $attributes['class'].
 		(option('curatescape_theme_fixes') ? ' curatescape-fix-'.$this->themeClass() : '').
 		' type-'.$this->typeClass();
