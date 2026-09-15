b604d0926a9fe9f875a195cf1854d134bcaec0fb
Erin Bell <ebellempire@gmail.com>
2026-09-15 14:02:20 -0400

    escape shortcode params

--- views/helpers/ShortcodeCuratescapeMap.php
+++ views/helpers/ShortcodeCuratescapeMap.php
@@ -3,8 +3,8 @@ class Curatescape_View_Helper_ShortcodeCuratescapeMap extends Zend_View_Helper_A
 	public function ShortcodeCuratescapeMap($args, $view, $html = null){
 		$includeSubjectsSelect = isset($args['subjects']) && boolval($args['subjects']) ? true : false;
 		$class = $includeSubjectsSelect ? 'shortcode-subjects' : 'shortcode-no-subjects';
-		$src = (isset($args['src']) && !$includeSubjectsSelect) ? plainText($args['src']) : WEB_ROOT.'/items/browse?output=mobile-json';
-		$figcaption = (isset($args['caption'])) ? plainText($args['caption']) : null;
+		$src = (isset($args['src']) && !$includeSubjectsSelect) ? html_escape(plainText($args['src'])) : WEB_ROOT.'/items/browse?output=mobile-json';
+		$figcaption = (isset($args['caption'])) ? html_escape(plainText($args['caption'])) : null;
 		// note that the subjects arg overrides custom src; subjects avail only for default/global src 
 		return get_view()->CuratescapeMap()->Multi($figcaption, true, $class, null, $src);
 	}
